00e2230f8027a12d4888af343ba12852726a66c8
Jéssica Neri <jess.neri94@gmail.com>
2020-10-20 20:12:53 -0300

    Front-end: tabela geral concluida

--- src/App.js
+++ src/App.js
@@ -1,24 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const legenda = {
+    nome: "Time",
+    pontos: "Pontos",
+    jogos: "Jogos",
+    vitorias: "Vitórias",
+    empates: "Empates",
+    derrotas: "Derrotas",
+    golsFeitos: "GF",
+    golsSofridos: "GS",
+  };
+
+  const colunas = [
+    "nome",
+    "pontos",
+    "empates",
+    "vitorias",
+    "derrotas",
+    "golsFeitos",
+    "golsSofridos",
+  ];
+
+  const [colunaOrdenada, setColunaOrdenada] = React.useState("pontos");
+  const [ordem, setOrdem] = React.useState("ascendente");
+  const [dados, setDados] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://desafio-3-back-cubos-academy.herokuapp.com/classificacao"
+    ).then(function (res) {
+      res.json().then(function (data) {
+        return setDados(data.dados);
+      });
+    });
+  }, []);
+
+  const dadosAscendentes = dados.sort((t1, t2) => {
+    //de onde eu vou pegar os dados para ordena-los?
+    if (
+      typeof t1[colunaOrdenada] === "number" &&
+      typeof t2[colunaOrdenada] === "number"
+    ) {
+      return (
+        parseInt(t2[colunaOrdenada], 10) - parseInt(t1[colunaOrdenada], 10)
+      );
+    } else {
+      return t1[colunaOrdenada].localeCompare(t2[colunaOrdenada]);
+    }
+  });
+
+  const dadosOrdenados =
+    ordem === "descendente" ? dadosAscendentes.reverse() : dadosAscendentes;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header-centro">
+          <h1>Brasileirão</h1>
+          <form className="login">
+            <label>
+              Email:
+              <input type="email"></input>
+            </label>
+            <label>
+              Senha:
+              <input type="password"></input>
+            </label>
+            <button>Logar</button>
+          </form>
+        </div>
       </header>
+      <main>
+        <div className="centro">
+          <div className="tabelaDeRodadas">
+            <div className="titulo">
+              <button>
+                <img
+                  src="https://systemuicons.com/images/icons/arrow_left.svg"
+                  alt="button"
+                ></img>
+              </button>
+              <h1>1ª rodada</h1>
+              <button>
+                <img
+                  src="https://systemuicons.com/images/icons/arrow_right.svg"
+                  alt="button"
+                ></img>
+              </button>
+            </div>
+            <div className="jogos">
+              <ul className="jogo">
+                <li className="time1">Grêmio</li>
+                <li className="gol1">1</li>
+                <li>x</li>
+                <li className="gol2">1</li>
+                <li className="time2">São Paulo</li>
+              </ul>
+              <ul className="jogo">
+                <li className="time1">Grêmio</li>
+                <li className="gol1">1</li>
+                <li>x</li>
+                <li className="gol2">1</li>
+                <li className="time2">São Paulo</li>
+              </ul>
+            </div>
+          </div>
+          <div className="tabelaDeTimes">
+            <table>
+              <thead>
+                <tr>
+                  <th>Posição</th>
+                  {colunas.map((coluna) => (
+                    <th>
+                      {legenda[coluna]}{" "}
+                      <button
+                        onClick={() => {
+                          if (colunaOrdenada === coluna) {
+                            setOrdem((ordem) =>
+                              ordem === "descendente"
+                                ? "descendente"
+                                : "ascendente"
+                            );
+                          } else {
+                            setColunaOrdenada(coluna);
+                            setOrdem("descendente");
+                          }
+                        }}
+                      >
+                        {colunaOrdenada !== coluna ||
+                        ordem === "descendente" ? (
+                          <img
+                            src="https://systemuicons.com/images/icons/arrow_down.svg"
+                            alt="bt"
+                          ></img>
+                        ) : (
+                          <img
+                            src="https://systemuicons.com/images/icons/arrow_up.svg"
+                            alt="bt"
+                          ></img>
+                        )}
+                      </button>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {dadosOrdenados.map((time, i) => (
+                  <tr>
+                    <td>{i + 1}</td>
+                    {colunas.map((coluna) => (
+                      <td>{time[coluna]}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
